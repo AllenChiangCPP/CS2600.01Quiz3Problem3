a570de2724d5b190af82bcd2521a63bdddb3a3d1
Allen <allenchiang@cpp.edu>
2022-10-20 21:35:26 -0700

    Created file for numberGuesser program and created outline for program

---
+++ numberGuesser.c
@@ -0,0 +1,15 @@
+#include <stdio.h> //this file is source code
+//guessingGame function, takes an int as the max number
+int guessingGame(int max)
+{
+    return 0;
+}
+int main() 
+{
+    //max number in main as an int
+    //encase entire program in while loop, should continue teh game if 1 or 2 is selected
+    //if else statements for picking 1:play game, 2: change max number, and 3: quit
+    //if 1 is picked: play game, function
+    //if 2 is picked: change max number in main and restart the loop
+    //if 3 is picked: exit loop and rest of the program
+} 
