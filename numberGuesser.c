de276da0532246d8e9d6b492df43086edbc1e87a
Allen <allenchiang@cpp.edu>
2022-10-22 21:11:48 -0700

    updated comments and fixed formatting of code

--- numberGuesser.c
+++ numberGuesser.c
@@ -1,4 +1,4 @@
-#include <stdio.h> //this file is source code
+#include <stdio.h> //this file is the source code
 #include <stdlib.h>
 #include <time.h>
 //guessingGame function, takes an int as the max number
@@ -6,12 +6,12 @@ int guessingGame(int max)
 {
     //randomly generates a number based on int max using srand and rand
     srand(time(NULL)); //function to make rand() not generate in a fixed pattern
-    int guessingNumber = rand() % max + 1; //generate a random number from 0-2 for the column row
+    int guessingNumber = rand() % max + 1; //generate a random number from 0-max as the number to be guessed
     printf("Guess the number. max: %d\n", max); //remind user max number inputted
     //encased in a while loop, exits and returns to menu in main on a win
     while(1) //while loop for the game
     {
-        char exitCheck; //char for checking for q to exit and ot stop chars from over printing
+        char exitCheck; //char for checking for q to exit 
         int userGuess; //int for storing user input
         scanf("%d", &userGuess); //get user input int
         exitCheck = getchar(); //getchar to catch character inputs and for checking for q input
@@ -39,9 +39,7 @@ int guessingGame(int max)
 }
 
 int main() 
-{
-    //max number in main as an int, intial value of 10
-    
+{   
     //use file to save and read in data
     FILE * saveMax;
     int maxNumber;
@@ -57,13 +55,14 @@ int main()
     //encase entire program in while loop, should continue the game if 1 or 2 is selected
     if(maxNumber < 0) //checks if maxNumber starts as a negative number, if so set to 10 to be default
     {
+        //max number in main as an int, intial value of 10
         maxNumber = 10;
     }
     while(gameRunning == 1)
     {
         while(1) //while loop for getting user input
         {
-            printf("Max Number currently: %d\n",maxNumber);
+            printf("Max Number currently: %d\n",maxNumber); //prints current maximum Number
             printf("Press 1 to play the game\nPress 2 to change the max number\nPress 3 to quit\n");
             int userGameInput; //int for storing user input
             scanf("%d", &userGameInput); //get user input
@@ -75,7 +74,7 @@ int main()
             printf("INVALID RESPONSE\n"); //print invalid response if user inputs an invalid answer
             getchar(); //prevent char input from ending program prematurely 
         }
-        //if else statements for picking 1:play game, 2: change max number, and 3: quit
+        //if else statements for picking 1: play game, 2: change max number, and 3: quit
         //if 1 is picked: play game, function
         if(userGamePick == 1)
         {
@@ -85,21 +84,21 @@ int main()
         if(userGamePick == 2)
         {
             while(1) //while loop for getting user input
-        {
-            int userNewMax; //int for storing user input
-            printf("Enter new maximum amount.(default: 10, max: 100)\n");
-            scanf("%d", &userNewMax); //get user input
-            if (userNewMax >= 0 && userNewMax <= 100) //check if user input is a 1,2,or 3
             {
-                maxNumber = userNewMax;
-                saveMax = fopen("saveMax.txt","w");
-                putw(userNewMax,saveMax);
-                fclose(saveMax);
-                break; //break out of loop to end question
+                int userNewMax; //int for storing user input
+                printf("Enter new maximum amount.(default: 10, max: 100)\n");
+                scanf("%d", &userNewMax); //get user input
+                if (userNewMax >= 0 && userNewMax <= 100) //check if user input is between 0 and 100 (max maximum amount)
+                {
+                    maxNumber = userNewMax; //set maxNumber to user inputted max amount
+                    saveMax = fopen("saveMax.txt","w"); //open saveMax.txt to write in
+                    putw(userNewMax,saveMax); //put user inputted max amount into text file
+                    fclose(saveMax);
+                    break; //break out of loop to end question
+                }
+                printf("INVALID RESPONSE\n"); //print invalid response if user inputs an invalid answer
+                getchar(); //prevent char input from ending program prematurely 
             }
-            printf("INVALID RESPONSE\n"); //print invalid response if user inputs an invalid answer
-            getchar(); //prevent char input from ending program prematurely 
-        }
         }
         //if 3 is picked: exit loop and rest of the program
         if(userGamePick == 3)
